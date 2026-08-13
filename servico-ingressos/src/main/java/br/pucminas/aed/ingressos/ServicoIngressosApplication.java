15bc1d65115469f0efa30ff6f91670573394eddb
255696 <gabrielcflisboa@gmail.com>
2026-08-13 13:45:44 -0300

    feat(ingressos): scaffold Maven do servico-ingressos (Spring Initializr)

---
+++ servico-ingressos/src/main/java/br/pucminas/aed/ingressos/ServicoIngressosApplication.java
@@ -0,0 +1,13 @@
+package br.pucminas.aed.ingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoIngressosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoIngressosApplication.class, args);
+	}
+
+}
---
+++ servico-ingressos/src/test/java/br/pucminas/aed/ingressos/ServicoIngressosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucminas.aed.ingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoIngressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
