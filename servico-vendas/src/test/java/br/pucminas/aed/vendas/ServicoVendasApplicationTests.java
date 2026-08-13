11ae2a45c0f16e457b3b262c35293bb3ffc907b9
255696 <gabrielcflisboa@gmail.com>
2026-08-13 13:45:37 -0300

    test(vendas): mantem teste padrao gerado pelo Spring Initializr

---
+++ servico-vendas/src/test/java/br/pucminas/aed/vendas/ServicoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucminas.aed.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
