e1a73676e7542c87ac35344cb6c0ea894c8b7f20
255696 <gabrielcflisboa@gmail.com>
2026-08-13 13:45:20 -0300

    feat(vendas): scaffold Maven do servico-vendas (Spring Initializr)

---
+++ servico-vendas/src/main/java/br/pucminas/aed/vendas/ServicoVendasApplication.java
@@ -0,0 +1,13 @@
+package br.pucminas.aed.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoVendasApplication.class, args);
+	}
+
+}
